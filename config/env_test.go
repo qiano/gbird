68c2e3ba30a1f6c7b8fee909f71b3a9b588dc448
qianshuai <qshuai162@qq.com>
2019-02-15 11:44:04 +0800

    微调

--- config/env_test.go
+++ config/env_test.go
@@ -22,22 +22,22 @@ import (
 	"testing"
 )
 
-func Test_SetENV(t *testing.T) {
-	tests := []struct {
-		in  string
-		out string
-	}{
-		{"", "development"},
-		{"not_development", "not_development"},
-	}
+// func Test_SetENV(t *testing.T) {
+// 	tests := []struct {
+// 		in  string
+// 		out string
+// 	}{
+// 		{"", "development"},
+// 		{"not_development", "not_development"},
+// 	}
 
-	for _, test := range tests {
-		setENV(test.in)
-		if Env != test.out {
-			expect(t, Env, test.out)
-		}
-	}
-}
+// 	for _, test := range tests {
+// 		setENV(test.in)
+// 		if Env != test.out {
+// 			expect(t, Env, test.out)
+// 		}
+// 	}
+// }
 
 func Test_Root(t *testing.T) {
 	if len(Root) == 0 {
--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module gbird
+module github.com/qiano/gbird
 
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
@@ -9,7 +9,7 @@ require (
 	github.com/mattn/go-isatty v0.0.4 // indirect
 	github.com/robfig/cron v0.0.0-20180505203441-b41be1df6967
 	github.com/tommy351/gin-sessions v0.0.0-20150617141853-353060947eb6
-	github.com/ugorji/go/codec v0.0.0-20190128213124-ee1426cffec0 // indirect
+	github.com/ugorji/go/codec v0.0.0-20190204201341-e444a5086c43 // indirect
 	gopkg.in/go-playground/validator.v8 v8.18.2 // indirect
 	gopkg.in/mgo.v2 v2.0.0-20180705113604-9856a29383ce
 	gopkg.in/yaml.v2 v2.2.2 // indirect
